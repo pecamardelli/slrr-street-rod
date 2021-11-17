0f492a53036aadc5858e90a741d63ecfea8185ce
Pablo Camardelli <pecamardelli@gmail.com>
2021-11-17 00:32:28 -0300

    * Add Chevy Camaro 68 colors * Fix Chevy Camaro 68 wheels and engine position * Fix GM V8 Pack engine positions

--- cars/racers/Focer_data/scripts/src/Chevrolet_Camaro_68_VT.java
+++ cars/racers/Focer_data/scripts/src/Chevrolet_Camaro_68_VT.java
@@ -7,6 +7,9 @@ public class Chevrolet_Camaro_68_VT extends VehicleType
 	public Chevrolet_Camaro_68_VT( int id )
 	{
 		VehicleModel vmd;
+		float coupeProduction = 1768.13;
+		float rsProduction = 409.77;
+		float ssProduction = 306.95;
 
 	// DEMO mode //
 		// Chevrolet Camaro SS Vinyl Top '68 //
@@ -57,104 +60,104 @@ public class Chevrolet_Camaro_68_VT extends VehicleType
 		// Chevrolet Camaro SS Vinyl Top '68 //
 			vmd=new VehicleModel( cars.racers.Focer:0x00000006r, VS_USED );
 			vtdarr.addElement( vmd );
-			vmd.prevalence = 2000.0;
+			vmd.prevalence = ssProduction*0.8;
 			vmd.minPower	= 0.5;	vmd.maxPower	= 1.5;
-			vmd.minOptical	= 0.5;	vmd.maxOptical	= 1.5;
+			vmd.minOptical	= 0.95;	vmd.maxOptical	= 1.5;
 			vmd.minTear	= 0.75;	vmd.maxTear	= 1.0;
 			vmd.minWear	= 0.75;	vmd.maxWear	= 1.0;
 			vmd.stockPrestige=397;	vmd.fullPrestige= 426;
 			vmd.stockQM = qm_stock_Shimutshibu_Focer_RC_200;	vmd.fullQM = qm_full_Shimutshibu_Focer_RC_200;
 			vmd.vehicleName = "Chevrolet Camaro SS Vinyl Top '68";
 			prevalence = vmd.prevalence;
-			vmd.addColorIndex(GameLogic.RID_CARCOLOR_Used_Rusty_Cherry);
-			vmd.addColorIndex(GameLogic.RID_CARCOLOR_Used_Rusty_Smaragd);
-			vmd.addColorIndex(GameLogic.RID_CARCOLOR_Used_Rusty_Nacht);
-			vmd.addColorIndex(GameLogic.RID_CARCOLOR_Used_Rusty_Zucker);
+			//vmd.addColorIndex(GameLogic.RID_CARCOLOR_Used_Rusty_Cherry);
+			//vmd.addColorIndex(GameLogic.RID_CARCOLOR_Used_Rusty_Smaragd);
+			//vmd.addColorIndex(GameLogic.RID_CARCOLOR_Used_Rusty_Nacht);
+			//vmd.addColorIndex(GameLogic.RID_CARCOLOR_Used_Rusty_Zucker);
 
 		// Chevrolet Camaro RS Vinyl Top '68 //
 			vmd=new VehicleModel( cars.racers.Focer:0x00002006r, VS_USED );
 			vtdarr.addElement( vmd );
-			vmd.prevalence = 1800.0;
+			vmd.prevalence = rsProduction*0.8;
 			vmd.minPower	= 0.5;	vmd.maxPower	= 1.5;
-			vmd.minOptical	= 0.5;	vmd.maxOptical	= 1.5;
+			vmd.minOptical	= 0.95;	vmd.maxOptical	= 1.5;
 			vmd.minTear	= 0.75;	vmd.maxTear	= 1.0;
 			vmd.minWear	= 0.75;	vmd.maxWear	= 1.0;
 			vmd.stockPrestige=475;	vmd.fullPrestige= 508;
 			vmd.stockQM = qm_stock_Shimutshibu_Focer_RC_200;	vmd.fullQM = qm_full_Shimutshibu_Focer_RC_200;
 			vmd.vehicleName = "Chevrolet Camaro RS Vinyl Top '68";
 			prevalence = vmd.prevalence;
-			vmd.addColorIndex(GameLogic.RID_CARCOLOR_Used_Rusty_Cherry);
-			vmd.addColorIndex(GameLogic.RID_CARCOLOR_Used_Rusty_Smaragd);
-			vmd.addColorIndex(GameLogic.RID_CARCOLOR_Used_Rusty_Nacht);
-			vmd.addColorIndex(GameLogic.RID_CARCOLOR_Used_Rusty_Zucker);
+			//vmd.addColorIndex(GameLogic.RID_CARCOLOR_Used_Rusty_Cherry);
+			//vmd.addColorIndex(GameLogic.RID_CARCOLOR_Used_Rusty_Smaragd);
+			//vmd.addColorIndex(GameLogic.RID_CARCOLOR_Used_Rusty_Nacht);
+			//vmd.addColorIndex(GameLogic.RID_CARCOLOR_Used_Rusty_Zucker);
 
 		// Chevrolet Camaro Vinyl Top '68 //
 			vmd=new VehicleModel( cars.racers.Focer:0x00003006r, VS_USED );
 			vtdarr.addElement( vmd );
-			vmd.prevalence = 1900.0;
+			vmd.prevalence = coupeProduction*0.8;
 			vmd.minPower	= 0.5;	vmd.maxPower	= 1.0;
-			vmd.minOptical	= 0.5;	vmd.maxOptical	= 1.0;
+			vmd.minOptical	= 0.95;	vmd.maxOptical	= 1.0;
 			vmd.minTear	= 0.75;	vmd.maxTear	= 0.9;
 			vmd.minWear	= 0.75;	vmd.maxWear	= 0.75;
 			vmd.stockPrestige=599;	vmd.fullPrestige= 632;
 			vmd.stockQM = qm_stock_Shimutshibu_Focer_RC_200;	vmd.fullQM = qm_full_Shimutshibu_Focer_RC_200;
 			vmd.vehicleName = "Chevrolet Camaro Vinyl Top '68";
 			prevalence = vmd.prevalence;
-			vmd.addColorIndex(GameLogic.RID_CARCOLOR_Used_Rusty_Cherry);
-			vmd.addColorIndex(GameLogic.RID_CARCOLOR_Used_Rusty_Smaragd);
-			vmd.addColorIndex(GameLogic.RID_CARCOLOR_Used_Rusty_Nacht);
-			vmd.addColorIndex(GameLogic.RID_CARCOLOR_Used_Rusty_Zucker);
+			//vmd.addColorIndex(GameLogic.RID_CARCOLOR_Used_Rusty_Cherry);
+			//vmd.addColorIndex(GameLogic.RID_CARCOLOR_Used_Rusty_Smaragd);
+			//vmd.addColorIndex(GameLogic.RID_CARCOLOR_Used_Rusty_Nacht);
+			//vmd.addColorIndex(GameLogic.RID_CARCOLOR_Used_Rusty_Zucker);
 			
 		// Chevrolet Camaro SS '68 //
 			vmd=new VehicleModel( cars.racers.Focer:0x00000107r, VS_USED );
 			vtdarr.addElement( vmd );
-			vmd.prevalence = 2000.0;
+			vmd.prevalence = ssProduction;
 			vmd.minPower	= 0.5;	vmd.maxPower	= 1.5;
-			vmd.minOptical	= 0.35;	vmd.maxOptical	= 1.5;
+			vmd.minOptical	= 0.95;	vmd.maxOptical	= 1.5;
 			vmd.minTear	= 0.75;	vmd.maxTear	= 1.0;
 			vmd.minWear	= 0.75;	vmd.maxWear	= 1.0;
 			vmd.stockPrestige=397;	vmd.fullPrestige= 426;
 			vmd.stockQM = qm_stock_Shimutshibu_Focer_RC_300;	vmd.fullQM = qm_full_Shimutshibu_Focer_RC_300;
 			vmd.vehicleName = "Chevrolet Camaro 396 '68";
 			prevalence = vmd.prevalence;
-			vmd.addColorIndex(GameLogic.RID_CARCOLOR_Used_Rusty_Cherry);
-			vmd.addColorIndex(GameLogic.RID_CARCOLOR_Used_Rusty_Smaragd);
-			vmd.addColorIndex(GameLogic.RID_CARCOLOR_Used_Rusty_Nacht);
-			vmd.addColorIndex(GameLogic.RID_CARCOLOR_Used_Rusty_Zucker);
+			//vmd.addColorIndex(GameLogic.RID_CARCOLOR_Used_Rusty_Cherry);
+			//vmd.addColorIndex(GameLogic.RID_CARCOLOR_Used_Rusty_Smaragd);
+			//vmd.addColorIndex(GameLogic.RID_CARCOLOR_Used_Rusty_Nacht);
+			//vmd.addColorIndex(GameLogic.RID_CARCOLOR_Used_Rusty_Zucker);
 
 		// Chevrolet Camaro RS '68 //
 			vmd=new VehicleModel( cars.racers.Focer:0x00002107r, VS_USED );
 			vtdarr.addElement( vmd );
-			vmd.prevalence = 1800.0;
+			vmd.prevalence = rsProduction;
 			vmd.minPower	= 0.5;	vmd.maxPower	= 1.5;
-			vmd.minOptical	= 0.35;	vmd.maxOptical	= 1.5;
+			vmd.minOptical	= 0.95;	vmd.maxOptical	= 1.5;
 			vmd.minTear	= 0.75;	vmd.maxTear	= 1.0;
 			vmd.minWear	= 0.75;	vmd.maxWear	= 1.0;
 			vmd.stockPrestige=475;	vmd.fullPrestige= 508;
 			vmd.stockQM = qm_stock_Shimutshibu_Focer_RC_300;	vmd.fullQM = qm_full_Shimutshibu_Focer_RC_300;
 			vmd.vehicleName = "Chevrolet Camaro RS '68";
 			prevalence = vmd.prevalence;
-			vmd.addColorIndex(GameLogic.RID_CARCOLOR_Used_Rusty_Cherry);
-			vmd.addColorIndex(GameLogic.RID_CARCOLOR_Used_Rusty_Smaragd);
-			vmd.addColorIndex(GameLogic.RID_CARCOLOR_Used_Rusty_Nacht);
-			vmd.addColorIndex(GameLogic.RID_CARCOLOR_Used_Rusty_Zucker);
+			//vmd.addColorIndex(GameLogic.RID_CARCOLOR_Used_Rusty_Cherry);
+			//vmd.addColorIndex(GameLogic.RID_CARCOLOR_Used_Rusty_Smaragd);
+			//vmd.addColorIndex(GameLogic.RID_CARCOLOR_Used_Rusty_Nacht);
+			//vmd.addColorIndex(GameLogic.RID_CARCOLOR_Used_Rusty_Zucker);
 
 		// Chevrolet Camaro '68 //
 			vmd=new VehicleModel( cars.racers.Focer:0x00003107r, VS_USED );
 			vtdarr.addElement( vmd );
-			vmd.prevalence = 2100.0;
+			vmd.prevalence = coupeProduction;
 			vmd.minPower	= 0.45;	vmd.maxPower	= 1.0;
-			vmd.minOptical	= 0.4;	vmd.maxOptical	= 1.0;
+			vmd.minOptical	= 0.95;	vmd.maxOptical	= 1.0;
 			vmd.minTear	= 0.65;	vmd.maxTear	= 0.9;
 			vmd.minWear	= 0.65;	vmd.maxWear	= 0.75;
 			vmd.stockPrestige=599;	vmd.fullPrestige= 632;
 			vmd.stockQM = qm_stock_Shimutshibu_Focer_RC_300;	vmd.fullQM = qm_full_Shimutshibu_Focer_RC_300;
 			vmd.vehicleName = "Chevrolet Camaro '68";
 			prevalence = vmd.prevalence;
-			vmd.addColorIndex(GameLogic.RID_CARCOLOR_Used_Rusty_Cherry);
-			vmd.addColorIndex(GameLogic.RID_CARCOLOR_Used_Rusty_Smaragd);
-			vmd.addColorIndex(GameLogic.RID_CARCOLOR_Used_Rusty_Nacht);
-			vmd.addColorIndex(GameLogic.RID_CARCOLOR_Used_Rusty_Zucker);
+			//vmd.addColorIndex(GameLogic.RID_CARCOLOR_Used_Rusty_Cherry);
+			//vmd.addColorIndex(GameLogic.RID_CARCOLOR_Used_Rusty_Smaragd);
+			//vmd.addColorIndex(GameLogic.RID_CARCOLOR_Used_Rusty_Nacht);
+			//vmd.addColorIndex(GameLogic.RID_CARCOLOR_Used_Rusty_Zucker);
 
 	// CAREER mode -> new car dealer //
 		// Chevrolet Camaro SS Vinyl Top '68 //
@@ -239,7 +242,7 @@ public class Chevrolet_Camaro_68_VT extends VehicleType
 		// Chevrolet Camaro SS Vinyl Top '68 //
 			vmd=new VehicleModel( cars.racers.Focer:0x00000006r, VS_DRACE );
 			vtdarr.addElement( vmd );
-			vmd.prevalence = 2100.0;
+			vmd.prevalence = ssProduction*1.2;
 			vmd.minPower	= 1.0;	vmd.maxPower	= 1.5;
 			vmd.minOptical	= 1.0;	vmd.maxOptical	= 1.5;
 			vmd.minTear	= 1.0;	vmd.maxTear	= 1.0;
@@ -251,7 +254,7 @@ public class Chevrolet_Camaro_68_VT extends VehicleType
 
 			vmd=new VehicleModel( cars.racers.Focer:0x00000006r, VS_NRACE );
 			vtdarr.addElement( vmd );
-			vmd.prevalence = 2000.0;
+			vmd.prevalence = ssProduction*0.8;
 			vmd.minPower	= 1.45;	vmd.maxPower	= 1.9;
 			vmd.minOptical	= 1.45;	vmd.maxOptical	= 1.9;
 			vmd.minTear	= 1.0;	vmd.maxTear	= 1.0;
@@ -263,7 +266,7 @@ public class Chevrolet_Camaro_68_VT extends VehicleType
 
 			vmd=new VehicleModel( cars.racers.Focer:0x00000006r, VS_RRACE );
 			vtdarr.addElement( vmd );
-			vmd.prevalence = 2000.0;
+			vmd.prevalence = ssProduction;
 			vmd.minPower	= 1.45;	vmd.maxPower	= 1.9;
 			vmd.minOptical	= 1.45;	vmd.maxOptical	= 1.9;
 			vmd.minTear	= 1.0;	vmd.maxTear	= 1.0;
@@ -276,7 +279,7 @@ public class Chevrolet_Camaro_68_VT extends VehicleType
 		// Chevrolet Camaro RS Vinyl Top '68 //
 			vmd=new VehicleModel( cars.racers.Focer:0x00002006r, VS_DRACE );
 			vtdarr.addElement( vmd );
-			vmd.prevalence = 2000.0;
+			vmd.prevalence = rsProduction*1.2;
 			vmd.minPower	= 1.0;	vmd.maxPower	= 1.5;
 			vmd.minOptical	= 1.0;	vmd.maxOptical	= 1.5;
 			vmd.minTear	= 1.0;	vmd.maxTear	= 1.0;
@@ -288,7 +291,7 @@ public class Chevrolet_Camaro_68_VT extends VehicleType
 
 			vmd=new VehicleModel( cars.racers.Focer:0x00002006r, VS_NRACE );
 			vtdarr.addElement( vmd );
-			vmd.prevalence = 2000.0;
+			vmd.prevalence = rsProduction;
 			vmd.minPower	= 1.5;	vmd.maxPower	= 2.0;
 			vmd.minOptical	= 1.5;	vmd.maxOptical	= 2.0;
 			vmd.minTear	= 1.0;	vmd.maxTear	= 1.0;
@@ -300,7 +303,7 @@ public class Chevrolet_Camaro_68_VT extends VehicleType
 			
 			vmd=new VehicleModel( cars.racers.Focer:0x00002006r, VS_RRACE );
 			vtdarr.addElement( vmd );
-			vmd.prevalence = 2000.0;
+			vmd.prevalence = rsProduction*0.8;
 			vmd.minPower	= 1.5;	vmd.maxPower	= 2.0;
 			vmd.minOptical	= 1.5;	vmd.maxOptical	= 2.0;
 			vmd.minTear	= 1.0;	vmd.maxTear	= 1.0;
@@ -313,7 +316,7 @@ public class Chevrolet_Camaro_68_VT extends VehicleType
 		// Chevrolet Camaro 350 Vinyl Top '68 //
 			vmd=new VehicleModel( cars.racers.Focer:0x00003006r, VS_DRACE );
 			vtdarr.addElement( vmd );
-			vmd.prevalence = 2000.0;
+			vmd.prevalence = coupeProduction*1.2;
 			vmd.minPower	= 1.0;	vmd.maxPower	= 1.7;
 			vmd.minOptical	= 1.0;	vmd.maxOptical	= 1.8;
 			vmd.minTear	= 1.0;	vmd.maxTear	= 1.0;
@@ -325,7 +328,7 @@ public class Chevrolet_Camaro_68_VT extends VehicleType
 
 			vmd=new VehicleModel( cars.racers.Focer:0x00003006r, VS_NRACE );
 			vtdarr.addElement( vmd );
-			vmd.prevalence = 2000.0;
+			vmd.prevalence = coupeProduction*1.2;
 			vmd.minPower	= 1.7;	vmd.maxPower	= 2.0;
 			vmd.minOptical	= 1.5;	vmd.maxOptical	= 2.0;
 			vmd.minTear	= 1.0;	vmd.maxTear	= 1.0;
@@ -337,7 +340,7 @@ public class Chevrolet_Camaro_68_VT extends VehicleType
 			
 			vmd=new VehicleModel( cars.racers.Focer:0x00003006r, VS_RRACE );
 			vtdarr.addElement( vmd );
-			vmd.prevalence = 2000.0;
+			vmd.prevalence = coupeProduction*1.2;
 			vmd.minPower	= 1.7;	vmd.maxPower	= 2.0;
 			vmd.minOptical	= 1.5;	vmd.maxOptical	= 2.0;
 			vmd.minTear	= 1.0;	vmd.maxTear	= 1.0;
@@ -350,7 +353,7 @@ public class Chevrolet_Camaro_68_VT extends VehicleType
 		// Chevrolet Camaro SS '68 //
 			vmd=new VehicleModel( cars.racers.Focer:0x00000107r, VS_DRACE );
 			vtdarr.addElement( vmd );
-			vmd.prevalence = 2000.0;
+			vmd.prevalence = ssProduction*1.25;
 			vmd.minPower	= 1.0;	vmd.maxPower	= 1.5;
 			vmd.minOptical	= 1.0;	vmd.maxOptical	= 1.5;
 			vmd.minTear	= 1.0;	vmd.maxTear	= 1.0;
@@ -362,7 +365,7 @@ public class Chevrolet_Camaro_68_VT extends VehicleType
 
 			vmd=new VehicleModel( cars.racers.Focer:0x00000107r, VS_NRACE );
 			vtdarr.addElement( vmd );
-			vmd.prevalence = 2000.0;
+			vmd.prevalence = ssProduction*1.25;
 			vmd.minPower	= 1.45;	vmd.maxPower	= 1.9;
 			vmd.minOptical	= 1.45;	vmd.maxOptical	= 1.9;
 			vmd.minTear	= 1.0;	vmd.maxTear	= 1.0;
@@ -374,7 +377,7 @@ public class Chevrolet_Camaro_68_VT extends VehicleType
 
 			vmd=new VehicleModel( cars.racers.Focer:0x00000107r, VS_RRACE );
 			vtdarr.addElement( vmd );
-			vmd.prevalence = 2000.0;
+			vmd.prevalence = ssProduction*1.25;
 			vmd.minPower	= 1.45;	vmd.maxPower	= 1.9;
 			vmd.minOptical	= 1.45;	vmd.maxOptical	= 1.9;
 			vmd.minTear	= 1.0;	vmd.maxTear	= 1.0;
@@ -387,7 +390,7 @@ public class Chevrolet_Camaro_68_VT extends VehicleType
 		// Chevrolet Camaro RS '68 //
 			vmd=new VehicleModel( cars.racers.Focer:0x00002107r, VS_DRACE );
 			vtdarr.addElement( vmd );
-			vmd.prevalence = 2000.0;
+			vmd.prevalence = rsProduction*1.24;
 			vmd.minPower	= 1.0;	vmd.maxPower	= 1.5;
 			vmd.minOptical	= 1.0;	vmd.maxOptical	= 1.5;
 			vmd.minTear	= 1.0;	vmd.maxTear	= 1.0;
@@ -399,7 +402,7 @@ public class Chevrolet_Camaro_68_VT extends VehicleType
 
 			vmd=new VehicleModel( cars.racers.Focer:0x00002107r, VS_NRACE );
 			vtdarr.addElement( vmd );
-			vmd.prevalence = 2000.0;
+			vmd.prevalence = rsProduction*1.24;
 			vmd.minPower	= 1.5;	vmd.maxPower	= 2.0;
 			vmd.minOptical	= 1.5;	vmd.maxOptical	= 2.0;
 			vmd.minTear	= 1.0;	vmd.maxTear	= 1.0;
@@ -411,7 +414,7 @@ public class Chevrolet_Camaro_68_VT extends VehicleType
 			
 			vmd=new VehicleModel( cars.racers.Focer:0x00002107r, VS_RRACE );
 			vtdarr.addElement( vmd );
-			vmd.prevalence = 2000.0;
+			vmd.prevalence = rsProduction*1.24;
 			vmd.minPower	= 1.5;	vmd.maxPower	= 2.0;
 			vmd.minOptical	= 1.5;	vmd.maxOptical	= 2.0;
 			vmd.minTear	= 1.0;	vmd.maxTear	= 1.0;
@@ -424,7 +427,7 @@ public class Chevrolet_Camaro_68_VT extends VehicleType
 		// Chevrolet Camaro '68 //
 			vmd=new VehicleModel( cars.racers.Focer:0x00003107r, VS_DRACE );
 			vtdarr.addElement( vmd );
-			vmd.prevalence = 2000.0;
+			vmd.prevalence = coupeProduction*1.2;
 			vmd.minPower	= 1.0;	vmd.maxPower	= 1.7;
 			vmd.minOptical	= 1.0;	vmd.maxOptical	= 1.8;
 			vmd.minTear	= 1.0;	vmd.maxTear	= 1.0;
@@ -436,7 +439,7 @@ public class Chevrolet_Camaro_68_VT extends VehicleType
 
 			vmd=new VehicleModel( cars.racers.Focer:0x00003107r, VS_NRACE );
 			vtdarr.addElement( vmd );
-			vmd.prevalence = 2000.0;
+			vmd.prevalence = coupeProduction*1.2;
 			vmd.minPower	= 1.7;	vmd.maxPower	= 2.0;
 			vmd.minOptical	= 1.5;	vmd.maxOptical	= 2.0;
 			vmd.minTear	= 1.0;	vmd.maxTear	= 1.0;
@@ -448,7 +451,7 @@ public class Chevrolet_Camaro_68_VT extends VehicleType
 			
 			vmd=new VehicleModel( cars.racers.Focer:0x00003107r, VS_RRACE );
 			vtdarr.addElement( vmd );
-			vmd.prevalence = 2000.0;
+			vmd.prevalence = coupeProduction*1.2;
 			vmd.minPower	= 1.7;	vmd.maxPower	= 2.0;
 			vmd.minOptical	= 1.5;	vmd.maxOptical	= 2.0;
 			vmd.minTear	= 1.0;	vmd.maxTear	= 1.0;
@@ -459,25 +462,26 @@ public class Chevrolet_Camaro_68_VT extends VehicleType
 			prevalence = vmd.prevalence;
 
 		// make color indexes //
-		addColorIndex(GameLogic.RID_CARCOLOR_Baiern_Devils_eye_red);
-		addColorIndex(GameLogic.RID_CARCOLOR_Baiern_Spring_yellow);
+		addColorIndex(GameLogic.CHEVROLET_TUXEDO_BLACK);
+		addColorIndex(GameLogic.CHEVROLET_CLASSIC_WHITE);
+		addColorIndex(GameLogic.CHEVROLET_GROTTO_BLUE);
+		addColorIndex(GameLogic.CHEVROLET_FATHOM_BLUE);
+		addColorIndex(GameLogic.CHEVROLET_ISLAND_TEAL);
+		addColorIndex(GameLogic.CHEVROLET_ASH_GOLD);
+		addColorIndex(GameLogic.CHEVROLET_GRECIAN_GREEN);
+		addColorIndex(GameLogic.CHEVROLET_RALLYE_GREEN);
+		addColorIndex(GameLogic.CHEVROLET_TRIPOLI_TURQUOISE);
+		addColorIndex(GameLogic.CHEVROLET_TEAL_BLUE);
+		addColorIndex(GameLogic.CHEVROLET_CORDOVAN_MAROON);
+		addColorIndex(GameLogic.CHEVROLET_CORVETTE_BRONZE);
+		addColorIndex(GameLogic.CHEVROLET_SEAFROST_GREEN);
+		addColorIndex(GameLogic.CHEVROLET_MATADOR_RED);
+		addColorIndex(GameLogic.CHEVROLET_PALOMINO_IVORY);
+		addColorIndex(GameLogic.CHEVROLET_LE_MANS_BLUE);
+		addColorIndex(GameLogic.CHEVROLET_SEQUOIA_GREEN);
+		addColorIndex(GameLogic.CHEVROLET_BUTTERNUT_YELLOW);
+		addColorIndex(GameLogic.CHEVROLET_BRITISH_GREEN);
 
-		addColorIndex(GameLogic.RID_CARCOLOR_Einvagen_Zucker);
-		addColorIndex(GameLogic.RID_CARCOLOR_Einvagen_Tornado_rot);
-		addColorIndex(GameLogic.RID_CARCOLOR_Einvagen_Nacht);
-		addColorIndex(GameLogic.RID_CARCOLOR_Einvagen_Smaragd);
-		addColorIndex(GameLogic.RID_CARCOLOR_Einvagen_Black_mage);
-		addColorIndex(GameLogic.RID_CARCOLOR_Einvagen_Hamvas_Grun);
-		addColorIndex(GameLogic.RID_CARCOLOR_Einvagen_Indigo);
-		addColorIndex(GameLogic.RID_CARCOLOR_Einvagen_Jazz);
-		addColorIndex(GameLogic.RID_CARCOLOR_Einvagen_Antracit);
-		addColorIndex(GameLogic.RID_CARCOLOR_Einvagen_Mercator_Blau);
-		addColorIndex(GameLogic.RID_CARCOLOR_Einvagen_Murano);
-		addColorIndex(GameLogic.RID_CARCOLOR_Einvagen_Champagner);
-		addColorIndex(GameLogic.RID_CARCOLOR_Einvagen_Ozean);
-		addColorIndex(GameLogic.RID_CARCOLOR_Einvagen_Reflex);
-		addColorIndex(GameLogic.RID_CARCOLOR_Einvagen_Saratoga);
-
-//		prevalence *= 1000.0;
+		prevalence *= 1000.0;
 	}
 }
